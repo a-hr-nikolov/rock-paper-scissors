edea09f65fb38dd16a23f72bced8931c10a03e08
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-10 09:58:26 +0200

    Add feature play best of five rock-paper-scissors

--- js/script.js
+++ js/script.js
@@ -29,5 +29,23 @@ function playRockPaperScissors(
       if (computerSelection === 'rock') return 'You lose! Rock beats Scissors!';
       if (computerSelection === 'paper')
         return 'You win! Scissors beats Paper!';
+    default:
+      return 'Not a valid choice, you lose!';
   }
+}
+
+function playRPSBestOfFive() {
+  let winnerTrack = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(
+      'What do you choose? (rock, paper, or scissors)'
+    );
+    let result = playRockPaperScissors(playerSelection);
+    console.log(result);
+    if (result.includes('win')) winnerTrack++;
+    if (result.includes('lose')) winnerTrack--;
+  }
+  if (winnerTrack == 0) return 'The PC is your equal!';
+  if (winnerTrack > 0) return 'The PC is no match for your wit!';
+  if (winnerTrack < 0) return 'You bring shame to the human race!';
 }
