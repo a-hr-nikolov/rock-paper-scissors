27fba7c333fa67b0f4185f138e5d7643a2bda81a
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-10 09:25:14 +0200

    Update getComputerChoice to return a word choice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// function getComputerChoice() {
+//   return Math.floor(Math.random() * 3) + 1;
+// }
+
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3) + 1;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  switch (randomNumber) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+  }
 }
+console.log(
+  '🚀 ~ file: script.js:6 ~ getComputerChoice ~ getComputerChoice():',
+  getComputerChoice()
+);
+
+// function roundRockPaperScissors(playerSelection, computerSelection) {
+//   let computerChoice = "";
+//   switch (computerSelection) {
+//     case 1:
+//   }
+// }
