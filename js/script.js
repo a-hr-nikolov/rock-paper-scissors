846e3b2c96fa6f46581ddd01c93c0daafedf8624
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-11 16:37:16 +0200

    Stylize the page and fix icon issues
    When there is an icon with the i tag within the button, for some reason
    clicking it is breaking the logic. It is considered the target, instead
    of the button being the target. This is fixed with this code in CSS:
    
    button > * {
      pointer-events: none;
    }

--- js/script.js
+++ js/script.js
@@ -12,19 +12,21 @@ function getComputerChoice() {
 
 function playRockPaperScissors(event) {
   let computerChoice = getComputerChoice();
-  let playerChoice = event.target.innerText.toLowerCase();
+  let playerChoice = event.target.value;
   let result;
   if (playerChoice === computerChoice) {
-    result = "It's a DRAW!";
+    result = 'The round is a DRAW, go again!';
   } else
     switch (playerChoice) {
       case 'rock':
-        if (computerChoice === 'paper') result = 'You lose! Paper beats Rock!';
+        if (computerChoice === 'paper')
+          result = 'You lose the round! Paper beats Rock!';
         if (computerChoice === 'scissors')
           result = 'You win! Rock beats Scissors!';
         break;
       case 'paper':
-        if (computerChoice === 'rock') result = 'You win! Paper beats Rock!';
+        if (computerChoice === 'rock')
+          result = 'You win the round! Paper beats Rock!';
         if (computerChoice === 'scissors')
           result = 'You lose! Scissors beats Paper!';
         break;
@@ -59,18 +61,14 @@ function keepScore(result) {
 
 function endGame() {
   if (document.querySelector('#player-score').innerText == 5) {
-    document.querySelector('#final-result').innerText =
-      'The PC is no match for your wit!';
-    buttonsRPS.forEach(button =>
-      button.removeEventListener('click', playRockPaperScissors)
-    );
+    document.querySelector('#result').innerText =
+      'THE GAME IS YOURS! No computer can match your wit!';
+    buttonsRPS.forEach(button => (button.style.display = 'none'));
   }
   if (document.querySelector('#computer-score').innerText == 5) {
-    document.querySelector('#final-result').innerText =
-      'You bring shame to the human race!';
-    buttonsRPS.forEach(button =>
-      button.removeEventListener('click', playRockPaperScissors)
-    );
+    document.querySelector('#result').innerText =
+      'YOU LOST... PATHETIC! You bring shame to the human race!';
+    buttonsRPS.forEach(button => (button.style.display = 'none'));
   }
 }
 
@@ -78,19 +76,3 @@ const buttonsRPS = document.querySelectorAll('.choice');
 buttonsRPS.forEach(button =>
   button.addEventListener('click', playRockPaperScissors)
 );
-
-// function playRPSBestOfFive() {
-//   let winnerTrack = 0;
-//   for (let i = 0; i < 5; i++) {
-//     let playerSelection = prompt(
-//       'What do you choose? (rock, paper, or scissors)'
-//     );
-//     let result = playRockPaperScissors(playerSelection);
-//     console.log(result);
-//     if (result.includes('win')) winnerTrack++;
-//     if (result.includes('lose')) winnerTrack--;
-//   }
-//   if (winnerTrack == 0) return 'The PC is your equal!';
-//   if (winnerTrack > 0) return 'The PC is no match for your wit!';
-//   if (winnerTrack < 0) return 'You bring shame to the human race!';
-// }
